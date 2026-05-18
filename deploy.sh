f85590fe10f16e343b9db77a5d289ce150f1f764
Asher Weisberger <asherweisberger@Mac-Studio.local>
2026-05-18 01:10:01 -0400

    Fix deploy script optional docs path

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,10 @@ if [ -z "$branch" ]; then
 fi
 
 echo "=== Committing and pushing to GitHub Pages branch: ${branch} ==="
-git add index.html aligned.html firebase.json unaligned_logo.png favicon.ico assets/docs flow-v4
+git add index.html flow-v4.html aligned.html firebase.json unaligned_logo.png favicon.ico flow-v4
+if [ -d assets/docs ]; then
+  git add assets/docs
+fi
 git commit -m "Update site" || echo "Nothing to commit"
 git push origin "HEAD:${branch}"
 
